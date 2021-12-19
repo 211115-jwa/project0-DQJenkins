3de6a67d9706123fbc2d8519a75fb096e67752d9
Dana Jenkins <94149057+DQJenkins@users.noreply.github.com>
2021-12-19 13:08:15 -0800

    gameDAOTest updated

--- src/test/java/com/revature/data/GameDAOTest.java
+++ src/test/java/com/revature/data/GameDAOTest.java
@@ -25,13 +25,128 @@ import java.util.Set;
 public class GameDAOTest {
 	private GameDAO gameDao = new GamePostgres();
 
-	//GamePostgres() methods to be tested
-	//Set<Product> getByName(String name)
-	//int create(Product dataToAdd)
-	//Product getById(int id)
-	//Set<Product> getAll()
-	//void update(Product dataToUpdate)
-	//void delete(Product dataToDelete)
-	//Set<Game> getByPlatform(String platform) 
-	//et<Game> getByPublisher(String publisher)
+/*GamePostgres() methods to be tested*/
+//Product getById(int id)
+	@Test
+	public void getByIdWhenIdExists() {
+		int idInput = 1;
+		Product gameOutput = gameDao.getById(idInput);
+		assertEquals(1, gameOutput.getId());
+	}
+
+	@Test
+	public void getByIdWhenIdDoesNotExists() {
+		int idInput = -1;
+		Product gameOutput = gameDao.getById(idInput);
+		assertNull(gameOutput);
+	}
+
+//Set<Product> getAll()
+	@Test
+	public void getAll() {
+		Set<Product> allOutput = gameDao.getAll();
+		assertNotNull(allOutput);
+	}
+
+//Set<Product> getByName(String name)
+	@Test
+	public void getByNameWhenNameExists() {
+		String name = "The";
+		Set<Product> nameOutput = gameDao.getByName(name);
+		nameOutput.forEach((Product item) -> {
+                assertTrue(item.getName().contains(name));
+        });
+	}
+	
+	//This test relies on getByName() and getAll() to both work
+	@Test
+	public void getByNameWhenNameIsBlank() {
+		String name = "";
+		Set<Product> blankNameOutput = gameDao.getByName(name);
+		assertEquals(gameDao.getAll(), blankNameOutput);
+	}
+	
+	@Test
+	public void getByNameWhenNameDoesNotExist() {
+		String name = "This Name Totally Doesn't Exist";
+		Set<Product> badNameOutput = gameDao.getByName(name);
+		assertNull(badNameOutput);
+	}
+	
+//int create(Product dataToAdd)
+	@Test
+	public void create() {
+		Product newGame = new Game();
+		int generatedId = gameDao.create(newGame);
+		assertNotEquals(0, generatedId);
+	}
+
+//void update(Product dataToUpdate)
+	@Test
+	public void update() {
+		Product gameToUpdate = gameDao.getById(1);
+		gameToUpdate.setName("New Name");
+		gameDao.update(gameToUpdate);
+		assertTrue(gameDao.getById(1).getName().contains("New Name"));
+	}
+	
+//void delete(Product dataToDelete)
+	@Test
+	public void delete() {
+		int currentSize = gameDao.getAll().size();
+		Product gameToDelete = gameDao.getById(1);
+		gameDao.delete(gameToDelete);
+		int newSize = gameDao.getAll().size();
+		assertEquals(currentSize-1, newSize);
+	}
+	
+//Set<Game> getByPlatform(String platform) 
+	@Test
+	public void getByPlatformWhenPlatformExists() {
+		String platform = "Playstation";
+		Set<Game> platformOutput = gameDao.getByPlatform(platform);
+		platformOutput.forEach((Game item) -> {
+                assertTrue(item.getPlatform().contains(platform));
+        });
+	}
+	
+	//This test relies on getByPlatform() and getAll() to both work
+	@Test
+	public void getByPlatformWhenPlatformIsBlank() {
+		String platform = "";
+		Set<Game> blankPlatformOutput = gameDao.getByPlatform(platform);
+		assertEquals(gameDao.getAll(), blankPlatformOutput);
+	}
+	
+	@Test
+	public void getByPlatformWhenPlatformDoesNotExist() {
+		String platform = "This Is Not a Real Platform";
+		Set<Game> badPlatformOutput = gameDao.getByPlatform(platform);
+		assertNull(badPlatformOutput);
+	}
+	
+//Set<Game> getByPublisher(String publisher)
+	@Test
+	public void getByPublisherWhenPublisherExists() {
+		String publisher = "Playstation";
+		Set<Game> publisherOutput = gameDao.getByPublisher(publisher);
+		publisherOutput.forEach((Game item) -> {
+                assertTrue(item.getPublisher().contains(publisher));
+        });
+	}
+	
+	//This test relies on getByPublisher() and getAll() to both work
+	@Test
+	public void getByPublisherWhenPublisherIsBlank() {
+		String publisher = "";
+		Set<Game> blankPublisherOutput = gameDao.getByPublisher(publisher);
+		assertEquals(gameDao.getAll(), blankPublisherOutput);
+	}
+	
+	@Test
+	public void getByPublisherWhenPublisherDoesNotExist() {
+		String publisher = "This Is Not a Real Publisher";
+		Set<Game> badPublisherOutput = gameDao.getByPublisher(publisher);
+		assertNull(badPublisherOutput);
+	}
 }
